237e477b44364cd90af6cf58dc3b9bbcc7803966
Joshua Christensen <joshua@codingwithjosh.com>
2022-05-31 17:07:04 -0500

    Change project structure

---
+++ Svelte-Movie-DB/.svelte-kit/generated/client-manifest.js
@@ -0,0 +1,17 @@
+export { matchers } from './client-matchers.js';
+
+export const components = [
+	() => import("../../src/routes/__layout.svelte"),
+	() => import("../runtime/components/error.svelte"),
+	() => import("../../src/routes/about.svelte"),
+	() => import("../../src/routes/index.svelte"),
+	() => import("../../src/routes/movie/[id].svelte"),
+	() => import("../../src/routes/search/[id].svelte")
+];
+
+export const dictionary = {
+	"": [[0, 3], [1]],
+	"about": [[0, 2], [1]],
+	"movie/[id]": [[0, 4], [1]],
+	"search/[id]": [[0, 5], [1]]
+};
---
+++ Svelte-Movie-DB/src/routes/index.svelte
@@ -0,0 +1,47 @@
+<!-- <script context="module" lang="ts"> is a SSR (Server Side Render) module -->
+<script context="module" lang="ts">
+  /// API Key
+  /**
+   * 7e1af67d95da889535e6fa84e56a36b1
+   */
+
+  /// Example API Request
+  /**
+   * https://api.themoviedb.org/3/movie/550?api_key=7e1af67d95da889535e6fa84e56a36b1
+   */
+
+  // export async function load<fetch>() {
+  export const load = async ({ fetch }) => {
+    // const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${}&language=en-US&page=1`)
+    const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=7e1af67d95da889535e6fa84e56a36b1&language=en-US&page=1`)
+    const data = await response.json()
+
+    if (response.ok) {
+      return {
+        props: {
+          popularMovies: data.results
+        }
+      }
+    }
+  }
+</script>
+
+<script lang="ts">
+  // Models
+  import type { Movie } from '../models/Movies'
+
+  // Components
+  import PopularMovies from '../components/PopularMovies.svelte'
+
+  // Run Time
+  import { fly } from 'svelte/transition'
+
+  export let popularMovies: Movie[]
+</script>
+
+<section in:fly={{ y: 50, duration: 500, delay: 500 }} out:fly={{ duration: 500 }}>
+  <PopularMovies {popularMovies} />
+</section>
+
+<style lang="less">
+</style>
